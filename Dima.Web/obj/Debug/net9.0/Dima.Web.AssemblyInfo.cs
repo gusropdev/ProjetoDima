d0c90af12faa68a24313ddfde15ff75fa7bda2ac
gusropdev <gusropdev@hotmail.com>
2025-04-08 19:07:09 -0300

    Inicial Commit

--- Dima.Web/obj/Debug/net9.0/Dima.Web.AssemblyInfo.cs
+++ Dima.Web/obj/Debug/net9.0/Dima.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a60469609a67a9e8a4efb97935b8b8c259f02b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
